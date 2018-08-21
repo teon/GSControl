7f8a2eeae5b5efbf5ec7b3fe89ae130e74055f3b
begreeen <michal.gumiela@gmail.com>
2018-08-21 22:40:41 +0200

    demodulator added to package

--- gnuradio/downlink/grc_part.py
+++ gnuradio/downlink/grc_part.py
@@ -1,6 +1,7 @@
 import argparse
 import source.file_source.run_with_file_browser as file_source
 import source.funcube_source.funcube_source as funcube_source
+import downlink as demodulator
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", required=False, default=False, action="store_true",
@@ -19,5 +20,8 @@ elif args.source == "pluto":
 elif args.source == "funcubeplus":
     print "Funcube plus"
     funcube_source.main()
+elif args.source == "demodulator":
+    print "demodulator"
+    demodulator.main()
 else:
     print "Any"
