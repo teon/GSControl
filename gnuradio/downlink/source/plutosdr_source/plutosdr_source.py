11af8a00d2a0f6d31b69255128a54a3ff85575a6
Michał Gumiela <michal.gumiela@gmail.com>
2018-08-24 18:10:12 +0200

    ZMQ intead of udp

--- gnuradio/downlink/source/plutosdr_source/plutosdr_source.py
+++ gnuradio/downlink/source/plutosdr_source/plutosdr_source.py
@@ -5,8 +5,8 @@
 #
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Funcube Source
-# Generated: Fri Aug 24 18:06:53 2018
+# Title: Plutosdr Source
+# Generated: Fri Aug 24 18:06:37 2018
 # GNU Radio version: 3.7.12.0
 ##################################################
 
@@ -22,23 +22,24 @@ if __name__ == '__main__':
 
 from PyQt4 import Qt
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
+from gnuradio import iio
 from gnuradio import zeromq
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
-import fcdproplus
 import sys
 from gnuradio import qtgui
 
 
-class funcube_source(gr.top_block, Qt.QWidget):
+class plutosdr_source(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Funcube Source")
+        gr.top_block.__init__(self, "Plutosdr Source")
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Funcube Source")
+        self.setWindowTitle("Plutosdr Source")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -56,15 +57,19 @@ class funcube_source(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "funcube_source")
+        self.settings = Qt.QSettings("GNU Radio", "plutosdr_source")
         self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
 
         ##################################################
         # Variables
         ##################################################
+        self.samp_rate = samp_rate = 192000
+        self.decimation = decimation = 3
+        self.rf_samp_rate = rf_samp_rate = samp_rate * decimation
         self.frequency_offset = frequency_offset = 20e3
         self.frequency = frequency = 435275000
+        self.Gain = Gain = 0
 
         ##################################################
         # Blocks
@@ -79,33 +84,62 @@ class funcube_source(gr.top_block, Qt.QWidget):
         self._frequency_line_edit.returnPressed.connect(
         	lambda: self.set_frequency(int(str(self._frequency_line_edit.text().toAscii()))))
         self.top_grid_layout.addWidget(self._frequency_tool_bar)
+        self._Gain_tool_bar = Qt.QToolBar(self)
+        self._Gain_tool_bar.addWidget(Qt.QLabel('Gain'+": "))
+        self._Gain_line_edit = Qt.QLineEdit(str(self.Gain))
+        self._Gain_tool_bar.addWidget(self._Gain_line_edit)
+        self._Gain_line_edit.returnPressed.connect(
+        	lambda: self.set_Gain(eng_notation.str_to_num(str(self._Gain_line_edit.text().toAscii()))))
+        self.top_grid_layout.addWidget(self._Gain_tool_bar)
         self.zeromq_pub_sink_0 = zeromq.pub_sink(gr.sizeof_gr_complex, 1, 'tcp://*:20001', 100, False, -1)
-        self.fcdproplus_fcdproplus_0 = fcdproplus.fcdproplus('',1)
-        self.fcdproplus_fcdproplus_0.set_lna(0)
-        self.fcdproplus_fcdproplus_0.set_mixer_gain(1)
-        self.fcdproplus_fcdproplus_0.set_if_gain(10)
-        self.fcdproplus_fcdproplus_0.set_freq_corr(0)
-        self.fcdproplus_fcdproplus_0.set_freq(frequency+frequency_offset)
-
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=decimation,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.pluto_source_0 = iio.pluto_source('ip:192.168.2.1', int(frequency+frequency_offset), rf_samp_rate, 20000000, 0x8000, True, True, True, "manual", Gain, '', True)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.fcdproplus_fcdproplus_0, 0), (self.zeromq_pub_sink_0, 0))
+        self.connect((self.pluto_source_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.zeromq_pub_sink_0, 0))
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "funcube_source")
+        self.settings = Qt.QSettings("GNU Radio", "plutosdr_source")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_rf_samp_rate(self.samp_rate * self.decimation)
+
+    def get_decimation(self):
+        return self.decimation
+
+    def set_decimation(self, decimation):
+        self.decimation = decimation
+        self.set_rf_samp_rate(self.samp_rate * self.decimation)
+
+    def get_rf_samp_rate(self):
+        return self.rf_samp_rate
+
+    def set_rf_samp_rate(self, rf_samp_rate):
+        self.rf_samp_rate = rf_samp_rate
+        self.pluto_source_0.set_params(int(self.frequency+self.frequency_offset), self.rf_samp_rate, 20000000, True, True, True, "manual", self.Gain, '', True)
 
     def get_frequency_offset(self):
         return self.frequency_offset
 
     def set_frequency_offset(self, frequency_offset):
         self.frequency_offset = frequency_offset
-        self.fcdproplus_fcdproplus_0.set_freq(self.frequency+self.frequency_offset)
+        self.pluto_source_0.set_params(int(self.frequency+self.frequency_offset), self.rf_samp_rate, 20000000, True, True, True, "manual", self.Gain, '', True)
 
     def get_frequency(self):
         return self.frequency
@@ -113,10 +147,18 @@ class funcube_source(gr.top_block, Qt.QWidget):
     def set_frequency(self, frequency):
         self.frequency = frequency
         Qt.QMetaObject.invokeMethod(self._frequency_line_edit, "setText", Qt.Q_ARG("QString", str(self.frequency)))
-        self.fcdproplus_fcdproplus_0.set_freq(self.frequency+self.frequency_offset)
+        self.pluto_source_0.set_params(int(self.frequency+self.frequency_offset), self.rf_samp_rate, 20000000, True, True, True, "manual", self.Gain, '', True)
+
+    def get_Gain(self):
+        return self.Gain
+
+    def set_Gain(self, Gain):
+        self.Gain = Gain
+        Qt.QMetaObject.invokeMethod(self._Gain_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.Gain)))
+        self.pluto_source_0.set_params(int(self.frequency+self.frequency_offset), self.rf_samp_rate, 20000000, True, True, True, "manual", self.Gain, '', True)
 
 
-def main(top_block_cls=funcube_source, options=None):
+def main(top_block_cls=plutosdr_source, options=None):
 
     from distutils.version import StrictVersion
     if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
